b7d7d1faf62e9a4784170c9a1b3f5a8697bfc87b
Devk1109 <devkundalwal@gmail.com>
2026-08-24 22:27:11 +0530

    modified:   src/components/Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -16,7 +16,7 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
     }
 
     // 🎂 Target Birthday
-    const targetDate = new Date("2026-07-06T00:00:00");
+    const targetDate = new Date("2026-08-25T00:00:00");
 
     const updateCountdown = () => {
       const now = new Date();
